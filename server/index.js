05821a92373a1e632b8fd6d8272f137011035846
user1990 <paulius.rimg1990@gmail.com>
2017-09-18 16:54:16 +0300

    # create-react-app start files

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, (err) => {
   if (err) { throw err; }
