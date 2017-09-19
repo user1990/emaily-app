ff27f8fb2636294465b67c4b6f174e147228ba47
user1990 <paulius.rimg1990@gmail.com>
2017-09-19 12:27:57 +0300

    # Added Redux store, react-router

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+// import './index.css';
+import App from './components/App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
